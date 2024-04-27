bf85f0b4123a14a563fa0fc18026451efcffbdfa
lilarin <80650911+lilarin@users.noreply.github.com>
2024-04-27 12:23:04 +0300

    Adding the ability to run multiple bots

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ with open('config.json', 'r', encoding='utf-8') as config_file:
 
 
 async def start_bot(bot_token, server_ip, server_query_port):
-    bot = commands.Bot(command_prefix="!!!", help_command=None, intents=Intents.all())
+    bot = commands.InteractionBot(intents=Intents.none())
 
     @tasks.loop(seconds=15)
     async def update_status():
         try:
             address = [server_ip, server_query_port]
-            a2s_info = a2s.info(tuple(address), timeout=5, encoding="utf-8")
+            a2s_info = a2s.info(tuple(address), timeout=15, encoding="utf-8")
 
             if a2s_info.player_count == 0:
                 activity_type = disnake.ActivityType.watching
@@ -30,8 +30,9 @@ async def start_bot(bot_token, server_ip, server_query_port):
                 status = disnake.Status.online
                 players_count = a2s_info.player_count
                 players_max_count = a2s_info.max_players
-                server_status = config["server_status"].format(players_count=players_count,
-                                                               players_max_count=players_max_count)
+                server_status = config["server_status"].format(
+                    players_count=players_count, players_max_count=players_max_count
+                )
                 await bot.change_presence(
                     activity=disnake.Activity(name=server_status, type=activity_type), status=status
                 )
